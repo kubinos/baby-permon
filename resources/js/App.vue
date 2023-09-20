a9f070b6ce122b8e86727e2a0169817013f5864a
Matěj Beran <matej.beran@icloud.com>
2023-09-20 20:26:11 +0200

    #1 Prepare frontend together with vue

---
+++ resources/js/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+const name = 'Vue 3';
+</script>
+
+<template>
+    <h1>Hello {{ name }}!</h1>
+</template>
+
+<style lang="scss">
+body {
+    background-color: black;
+}
+
+h1 {
+    color: white;
+    text-align: center;
+}
+</style>
