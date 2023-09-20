1a68cfec11395756e8f7a10f70a4825e7578578f
Matěj Beran <matej.beran@icloud.com>
2023-09-20 20:40:34 +0200

    #1 Final adjustments with documentation

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
+// reference: https://laravel.com/docs/10.x/vite#vue
+
 export default defineConfig({
     plugins: [
         laravel(['resources/js/app.js']),
